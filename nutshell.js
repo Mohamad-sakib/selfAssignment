c24acad48bfa9fc854d401d907f8ab1144b5cebf
Mohamad-sakib <158846349+Mohamad-sakib@users.noreply.github.com>
2024-12-19 17:11:06 +0530

    echo function added

--- nutshell.js
+++ nutshell.js
@@ -1,16 +1,46 @@
 let path = "null shell %";
 
-const inBuildCommands = [["echo", echo]];
 const echo = function (args) {
+  const stringToReturn = args.join(" ");
+  
+  if (stringToReturn[0] === '"' || stringToReturn[0] === "'") {
+    return stringToReturn.slice(1, stringToReturn.length - 1);
+  }
 
+  return  stringToReturn;
 };
 
-const runCommand  = function () {
-  
+const builtInCommands = [["echo", echo]];
+// const extermalCommands = [["deno", deno]];
+
+
+
+// const isExternalCommand = function (userCommand, args) {
+//   for (const command of builtInCommands) {
+//     if (command === userCommand) {
+//       return userCommand(args);
+//     }
+//   }
+// };
+
+const runCommand = function (userCommand, args) {
+  for (const command of builtInCommands) {
+    if (command[0] === userCommand) {
+      return command[1](args);
+    };
+  }
+
+  return "invalid command";
+  // return isExternalCommand(userCommand, args);
+}
+
+const displayResult = function (result) {
+  console.log(result);
 }
 
 while (true) {
   const instruction = prompt(path);
   const [command, ...args] = instruction.split(" ");
   const result = runCommand(command, args);
+  displayResult(result);
 }
